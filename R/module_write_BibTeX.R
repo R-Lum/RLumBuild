a5c81d88ce136c210eead314f70b897cd06a8f54
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2019-05-02 17:39:15 +0200

    Add module_write_BibTeX()

---
+++ R/module_write_BibTeX.R
@@ -0,0 +1,24 @@
+
+#' Create Package BibTeX file
+#'
+#' @author Sebastian Kreutzer, IRAMAT-CRP2A, UMR 5060, CNRS - Université Bordeaux Montaigne (France)
+#'
+#'
+#' @section Function version: 0.1.0
+#'
+#' @export
+module_write_BibTeX <- function(){
+
+##get version number and package name
+temp <- readLines("DESCRIPTION")
+
+pkg_name <- trimws(unlist(strsplit(temp[1], split = "Package:", fixed = TRUE))[2])
+pkg_version <- trimws(
+  unlist(strsplit(temp[grepl(pattern = "Version:", x = temp, fixed = TRUE)], split = "Version:", fixed = TRUE))[2])
+
+package.citation <- utils::toBibtex(citation(pkg_name))
+write(package.citation, file=paste0(pkg_name,".BuildResults/",pkg_name,"_", pkg_version,"-bibliography.bib"))
+
+return(TRUE)
+
+}
