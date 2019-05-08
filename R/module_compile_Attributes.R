6c56da3ccb64c63ab2510d0f5fdb604239e6d780
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2019-05-08 22:29:26 +0200

    Update compile attributes

--- R/module_compile_Attributes.R
+++ R/module_compile_Attributes.R
@@ -1,5 +1,13 @@
 #' Module: Compile attributes
 #'
+#' @description Compile C++ attributes if such code is part of the package
+#'
+#' @author Sebastian Kreutzer, IRAMAT-CRP2A, UMR 5060, CNRS - Université Bordeaux Montaigne (Frange)
+#'
+#' @section Function version: 0.1.0
+#'
+#'
+#' @md
 #' @export
 module_compile_Attributes <- function() {
   Rcpp::compileAttributes()
