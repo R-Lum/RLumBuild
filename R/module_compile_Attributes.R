27e3b90fd54ccf32ad2cde615c0416a5a42e1afc
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2019-05-08 22:35:16 +0200

    Typo

--- R/module_compile_Attributes.R
+++ R/module_compile_Attributes.R
@@ -2,7 +2,7 @@
 #'
 #' @description Compile C++ attributes if such code is part of the package
 #'
-#' @author Sebastian Kreutzer, IRAMAT-CRP2A, UMR 5060, CNRS - Université Bordeaux Montaigne (Frange)
+#' @author Sebastian Kreutzer, IRAMAT-CRP2A, UMR 5060, CNRS - Université Bordeaux Montaigne (France)
 #'
 #' @section Function version: 0.1.0
 #'
