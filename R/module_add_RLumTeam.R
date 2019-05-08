d4e8873278603fdbe433047fffe12b8e7d1639e6
RLumSK <sebastian.kreutzer@u-bordeaux-montaigne.fr>
2019-05-08 22:21:11 +0200

    Update module_add_RLumTeam() and remove version timestamps.

--- R/module_add_RLumTeam.R
+++ R/module_add_RLumTeam.R
@@ -1,73 +1,47 @@
 #' Module: add RLum-Team and update version timestamp
 #'
-#' @author Sebastian Kreutzer
+#' @description If the package was supported by the RLumTeam (the standard) the RLum-Team is added
+#' as author to all functions.
+#'
+#' @author Sebastian Kreutzer, IRAMAT-CRP2A, UMR 5060, CNRS - Université Bordeaux Montaigne (Frange)
 #'
 #' @section Function version: 0.1.0
 #'
 #' @md
 #' @export
 module_add_RLumTeam <- function() {
-  # Reading file ------------------------------------------------------------
 
-  file.list.man <- list.files("man/", recursive = TRUE, include.dirs = FALSE)
-  file.list.R <- list.files("R/")
+  # Reading file ------------------------------------------------------------
+  file.list.man <- list.files("man/", recursive = TRUE, include.dirs = FALSE, pattern = "\\.Rd",
+                              full.names = TRUE)
 
   # Adding additional information ---------------------------------------------------------------
 
   ##Adding change time time stamp and the R Luminescence team to as package author
-  for (i in 1:length(file.list.man)) {
-    temp.file.man <-  readLines(paste0("man/",file.list.man[i]))
+  for (i in file.list.man) {
+    temp.file.man <-  readLines(i, warn = FALSE)
 
-    ##seach for start and end author field
-    author.start <- which(grepl("\\\\author",temp.file.man))
-    version.start <-
-      which(grepl("\\\\section\\{Function version\\}",temp.file.man))
+    ##seach for start field
+    author.start <- which(grepl("\\author{",temp.file.man, fixed = TRUE))
 
     ##add luminescence team as author
     ##to avoid that the field was not found
-    if (length(author.start) > 0) {
-      author.end <- min(which(grepl(
-        x = temp.file.man[author.start + 1:length(temp.file.man)],
-        pattern = "}",fixed = TRUE)) + author.start)
+    if (length(author.start)) {
+
+      ##search for the next curly braces
+      temp_id <- grep(pattern = "}", x = temp.file.man, fixed = TRUE)
+      author.end <- min(temp_id[temp_id > author.start])
 
       ##replace this entry
-      temp.file.man[author.end] <-
-        "\\cr R Luminescence Package Team}"
-
+      temp.file.man[author.end] <- "\\cr RLum Developer Team}"
 
     }
 
-    ##add version time stamp
-    if (length(version.start) > 0) {
-      ##check if a correspoding R files exists
-      if (any(grepl(paste0(
-        strsplit(file.list.man[i], split = ".Rd")[[1]], ".R"
-      ), file.list.R))) {
-        ##select the corresponding R file (the limitation to the 1st element to avoid problems with
-        ## the RLum.Results and the RLum.R file)
-        temp.id <-
-          which(grepl(paste0(
-            strsplit(file.list.man[i], split = ".Rd")[[1]], ".R"
-          ), file.list.R))[1]
-
-        ##select mtime
-        temp.mtime <-
-          file.info(paste0("R/",file.list.R[temp.id]))$mtime
-
-        ##update information
-        temp.file.man[version.start + 1] <-
-          paste0(temp.file.man[version.start + 1], " (",  temp.mtime, ")")
-
-      }
-
-    }
-
-    ##write file back to the disc
-    if (length(version.start) > 0 || length(author.start) > 0) {
-      write(temp.file.man,paste0("man/",file.list.man[i]))
-    }
+    ##write file
+    writeLines(temp.file.man, con = i)
 
   }
+
 
   return(TRUE)
 
